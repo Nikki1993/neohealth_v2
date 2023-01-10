6fc9425d3fed4c7961bbe076a9174fdedac5c14a
Nick Kononov <nikitakononov93@gmail.com>
2023-01-10 23:01:47 +0200

    Lazy load images Do not show directory listing

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
-	"os"
+	"path/filepath"
 )
 
 var staticPath = "/static/"
 
+//go:embed static/*
+var content embed.FS
+
+//go:embed all:templates/*
+var templates embed.FS
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
 func main() {
-	static := http.StripPrefix(staticPath, http.FileServer(http.Dir(fmt.Sprintf(".%s", staticPath))))
+	cont, err := fs.Sub(fs.FS(content), "static")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	static := http.StripPrefix(staticPath, http.FileServer(neuteredFileSystem{http.FS(cont)}))
 
 	http.Handle(staticPath, static)
 	http.HandleFunc("/", servePage)
 
-	err := http.ListenAndServe(":1234", logRequest(http.DefaultServeMux))
+	err = http.ListenAndServe(":1234", logRequest(http.DefaultServeMux))
 	if err != nil {
 		log.Fatalln("Error starting server on port :1234")
 	}
@@ -42,16 +59,16 @@ func servePage(w http.ResponseWriter, r *http.Request) {
 func parseTemplates() (*template.Template, error) {
 	var filePaths []string
 
-	files, err := os.ReadDir("./templates")
+	files, err := fs.ReadDir(templates, "templates")
 	if err != nil {
 		return nil, err
 	}
 
 	for _, file := range files {
-		filePaths = append(filePaths, fmt.Sprintf("./templates/%v", file.Name()))
+		filePaths = append(filePaths, fmt.Sprintf("templates/%v", file.Name()))
 	}
 
-	tmpl, err := template.ParseFiles(filePaths...)
+	tmpl, err := template.ParseFS(templates, filePaths...)
 	if err != nil {
 		return nil, err
 	}
@@ -64,4 +81,30 @@ func logRequest(handler http.Handler) http.Handler {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
 	})
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
