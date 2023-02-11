335e364db66464bac079062ab14dbbc701c759b9
Nick Kononov <nikitakononov93@gmail.com>
2023-02-11 16:02:09 +0200

    Build system fixes

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 hash=$(sha256sum main.css | cut -c1-64)
-npx tailwindcss -i main.css -o ./static/style."${hash}".css --minify && go build -o ./build/neohealth .
+rm ./static/*.css
+rm -rf build/
+npx tailwindcss -i main.css -o ./static/style."${hash}".css --minify
+go build -o ./build/neohealth .
+rm ./static/*.css
