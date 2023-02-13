9624e9cc3f43babcbdc9e4cbcc8bc2243556951f
Nick Kononov <nikitakononov93@gmail.com>
2023-02-13 21:20:29 +0200

    Updated build script and manifest theme

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 hash=$(sha256sum main.css | cut -c1-64)
-rm ./static/*.css
+rm -f ./static/*.css
 rm -rf build/
 npx tailwindcss -i main.css -o ./static/style."${hash}".css --minify
 go build -o ./build/neohealth .
